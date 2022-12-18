fc3691f000a097fd98d622f2644dc5ec9449a65a
Rehman_tyagi <irontyagi4@gmail.com>
2022-12-19 04:46:45 +0530

    changed

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import "./App.css";
 import TopHeader from "./components/topHeader";
 import Navbar from "./components/navbar";
 import HeroSlider from "./components/heroSlider";
-import Spinner from "./components/spinner";
 function App() {
   return (
     <div>
--- src/components/heroSlider.js
+++ src/components/heroSlider.js
@@ -4,7 +4,7 @@ import WaterDropAnimation from "./waterDrop";
 const HeroSlider = () => {
   return (
     <div className="poster-wrapper">
-      <img src={Poster} className="hero-poster" />
+      <img src={Poster} alt="poster" className="hero-poster" />
       <div className="hero-poster_text__content">
         <h1>Don't worry about your waste-water, We are for you!</h1>
         <p>A fully trusted brand in the industry by its incredible services.</p>
