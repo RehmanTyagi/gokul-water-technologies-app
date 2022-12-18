325dc216e593d7c208a1af0e8ce998ac33c997b4
Rehman_tyagi <irontyagi4@gmail.com>
2022-12-19 04:43:22 +0530

    hero section started

---
+++ src/components/heroSlider.js
@@ -0,0 +1,15 @@
+import "./heroSlider.css";
+import Poster from "../assets/imgs/etp-plant.png";
+import WaterDropAnimation from "./waterDrop";
+const HeroSlider = () => {
+  return (
+    <div className="poster-wrapper">
+      <img src={Poster} className="hero-poster" />
+      <div className="hero-poster_text__content">
+        <h1>Don't worry about your waste-water, We are for you!</h1>
+        <p>A fully trusted brand in the industry by its incredible services.</p>
+      </div>
+    </div>
+  );
+};
+export default HeroSlider;
---
+++ src/components/waterDrop.js
@@ -0,0 +1,22 @@
+import "./waterDrop.css";
+
+const WaterDropAnimation = () => {
+  return (
+    <section class="cards">
+      <div class="container">
+        <div class="drop">
+          <h2>Card Title</h2>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptas,
+            voluptatem.
+          </p>
+          <a class="read-btn" href="#">
+            Read More
+          </a>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default WaterDropAnimation;
