cd84ae3b790eae97ba203286d245f70a4b63032e
Rehman_tyagi <irontyagi4@gmail.com>
2022-12-20 02:10:16 +0530

    started service sec

--- src/components/services/products.js
+++ src/components/services/products.js
@@ -4,7 +4,7 @@ import img from "../../assets/imgs/etp-plant.png";
 const Product = () => {
   return (
     <div className="product-wrapper">
-      <img src={img} />
+      <img alt="img" src={img} />
       <h2>ETP Module</h2>
       <p>ETP plant for Effluent Treatment.</p>
       <button>Send Query</button>
