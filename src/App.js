77df789076d069403cdc97e9512bb4f4b16996b7
Rehman_tyagi <irontyagi4@gmail.com>
2023-01-05 21:41:25 +0530

    made maintenance message

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import "./App.css";
-import TopHeader from "./components/topHeader";
-import Navbar from "./components/navbar";
-import HeroSlider from "./components/heroSlider";
-import Services from "./components/services/services";
-import ClientSection from "./components/clients/clients-section";
-import CompanyMileStone from "./components/company-milestone";
+// import TopHeader from "./components/topHeader";
+// import Navbar from "./components/navbar";
+// import HeroSlider from "./components/heroSlider";
+// import Services from "./components/services/services";
+// import ClientSection from "./components/clients/clients-section";
+// import CompanyMileStone from "./components/company-milestone";
 import DevelopMessage from "./components/developmentmessage/developmentmessage";
 function App() {
   return (
